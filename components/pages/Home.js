c64e2dc6fb0f7df57f6c330c56abdc468de175e0
dubbs1515 <dubbsware@gmail.com>
2021-07-27 17:31:24 -0700

    config

---
+++ components/pages/Home.js
@@ -0,0 +1,10 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+
+const Home = () => {
+    <div className='home'>
+        <h1>Narrative Visualization</h1>
+    </div>
+}
+
+export default Home;
